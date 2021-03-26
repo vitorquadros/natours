c31206136c1a24cb00c03884a9f070d7d17961bc
vitor <viitorquadros@hotmail.com>
2021-03-25 22:51:41 -0300

    feat: create new tour

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 
 const app = express();
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -14,6 +14,20 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours,
     },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (error) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
   });
 });
 
