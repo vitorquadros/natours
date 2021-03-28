85294fb760d98baac3df1600ab9c52a37ee59e8b
vitor <viitorquadros@hotmail.com>
2021-03-28 20:37:53 -0300

    refactor (routes): server file

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./src/routes/tourRoutes');
+const userRouter = require('./src/routes/userRoutes');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,4 @@
-const express = require('express');
-const morgan = require('morgan');
-const tourRouter = require('./src/routes/tourRoutes');
-const userRouter = require('./src/routes/userRoutes');
-
-const app = express();
-
-app.use(morgan('dev'));
-app.use(express.json());
-
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/tours', userRouter);
+const app = require('./app');
 
 app.listen(3000, () => {
   console.log('Running...');
