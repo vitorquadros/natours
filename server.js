e19586d91d1750cc0346a268b010cc4c100b9061
vitor <viitorquadros@hotmail.com>
2021-03-28 18:51:27 -0300

    feat: get tour by id

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const express = require('express');
+const { ppid } = require('process');
 
 const app = express();
 
 app.use(express.json());
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -17,18 +20,39 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params;
+  const tour = tours.find((el) => el.id === Number(id));
+  if (tour) {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: 'not found',
+    });
+  }
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (error) => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  });
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(3000, () => {
