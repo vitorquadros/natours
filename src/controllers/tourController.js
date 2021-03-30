9b30b3ba01afd673e6af9a6cb8d10feee881172f
vitor <viitorquadros@hotmail.com>
2021-03-30 16:20:33 -0300

    removing fake data logic

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -1,18 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../../dev-data/data/tours-simple.json`)
-);
-
-exports.checkId = (req, res, next, value) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'not found',
-      message: 'invalid id',
-    });
-  }
-  next();
-};
+const Tour = require('../models/TourModel');
 
 exports.checkBody = (req, res, next) => {
   const { name, price } = req.body;
@@ -30,40 +16,31 @@ exports.checkBody = (req, res, next) => {
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   const { id } = req.params;
-  const tour = tours.find((el) => el.id === Number(id));
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // const tour = tours.find((el) => el.id === Number(id));
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (error) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.updateTour = (req, res) => {
