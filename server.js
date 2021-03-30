be16552a31cf2c2ed58f750c640d2b2618fbdefe
vitor <viitorquadros@hotmail.com>
2021-03-30 15:44:35 -0300

    db connection with mongoose

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+// MONGODB
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+// ROUTES
 const app = require('./app');
 
+// STARTING SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Running on ${process.env.NODE_ENV} mode...`);
