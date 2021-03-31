b9ae9fa8f0455ee30d6550947c26d99c39e6f509
vitor <viitorquadros@hotmail.com>
2021-03-30 22:22:49 -0300

    feat: script to import fake json data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+const Tour = require('../../src/models/TourModel');
+
+dotenv.config({ path: './config.env' });
+
+// MONGODB
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+// READ JSON FILE
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM COLLECTION
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] == '--delete') {
+  deleteData();
+}
