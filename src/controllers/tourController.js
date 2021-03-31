fda6acfd0be3f8f32d0bc61e6c8c99a4746ce337
vitor <viitorquadros@hotmail.com>
2021-03-30 22:45:20 -0300

    feat (tourController): getAllTours query filter

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -2,7 +2,12 @@ const Tour = require('../models/TourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
