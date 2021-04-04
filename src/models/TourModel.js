bfb6ee619e9bc07cccfae2389240167b1f20507f
vitor <viitorquadros@hotmail.com>
2021-04-04 19:52:09 -0300

    feat (TourModel): custom validators

--- src/models/TourModel.js
+++ src/models/TourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -42,7 +43,15 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (value) {
+          return value < this.price;
+        },
+        message: 'Discount price should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
