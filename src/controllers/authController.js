c3356e7132959a7c31f23720b66a85ea3a8f51f3
vitor <viitorquadros@hotmail.com>
2021-04-07 19:33:30 -0300

    feat (authController): create signup

---
+++ src/controllers/authController.js
@@ -0,0 +1,13 @@
+const User = require('../models/UserModel');
+const catchAsync = require('../Utils/catchAsync');
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
+});
