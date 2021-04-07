69f038efd36ae260a536433b5a7ee6179791fa4b
vitor <viitorquadros@hotmail.com>
2021-04-07 20:00:23 -0300

    feat (UserModel): password confirm validation

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -22,6 +22,13 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // this only works on CREATE or SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords do not match',
+    },
   },
 });
 
