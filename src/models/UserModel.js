f6bffe783a6d86e0b67a8266948d5dd0f5cac793
vitor <viitorquadros@hotmail.com>
2021-04-12 18:25:20 -0300

    feat (userController): delete user

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -41,6 +41,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 // pre-save middleware
@@ -58,6 +63,11 @@ userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
   next();
 });
 
