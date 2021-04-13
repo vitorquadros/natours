f0d90e5b859a478e0cbbb07b71a1b86f1cdaacf2
vitor <viitorquadros@hotmail.com>
2021-04-12 21:07:14 -0300

    feat (app): prevent parameter polution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./src/routes/tourRoutes');
 const userRouter = require('./src/routes/userRoutes');
@@ -39,6 +40,20 @@ app.use(mongoSanitize());
 
 // data sanitization against XSS
 app.use(xss());
+
+// prevent parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // serving static files
 app.use(express.static(`${__dirname}/public`));
