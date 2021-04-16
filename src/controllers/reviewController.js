cd68b009230cb2e79c86fac3661ff5d9cfe675aa
vitor <viitorquadros@hotmail.com>
2021-04-16 17:51:47 -0300

    feat (reviewController): get review (nested)

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -2,7 +2,10 @@ const catchAsync = require('../Utils/catchAsync');
 const Review = require('../models/ReviewModel');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
