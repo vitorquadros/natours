d62aaa0d370d37d5e78f1dca07ae53fdd264feda
vitor <viitorquadros@hotmail.com>
2021-04-16 17:35:55 -0300

    feat (reviewRouter): create review (nested)

--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const router = express.Router();
 
@@ -27,5 +28,13 @@ router
   .route('/')
   .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
 
 module.exports = router;
