dc4e1693113fcf649660697db72bae0f47bfa48a
vitor <viitorquadros@hotmail.com>
2021-04-16 18:22:57 -0300

    refactor: create function

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -17,20 +17,15 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   // allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
 
-  const newReview = await Review.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      newReview,
-    },
-  });
-});
+exports.createReview = factory.createOne(Review);
 
 exports.updateReview = factory.updateOne(Review);
+
 exports.deleteReview = factory.deleteOne(Review);
