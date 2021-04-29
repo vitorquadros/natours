59df9a35a313e0aa01792ec3ad92137c73f9f151
vitor <viitorquadros@hotmail.com>
2021-04-29 15:25:33 -0300

    feat (user controller): get current user middleware/function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -56,6 +56,11 @@ exports.createUser = (req, res) => {
   });
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.getAllUsers = factory.getAll(User);
 
 exports.getUser = factory.getOne(User);
