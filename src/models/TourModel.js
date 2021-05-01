b902edf8d00f301d26966ab617c78a2151472680
vitor <viitorquadros@hotmail.com>
2021-04-30 21:41:15 -0300

    feat (tourController): get tours distances

--- src/models/TourModel.js
+++ src/models/TourModel.js
@@ -158,10 +158,10 @@ tourSchema.pre(/^find/, function (next) {
 });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
