593154270ab503c67f52c8b257fc4776fec656c8
vitor <viitorquadros@hotmail.com>
2021-05-04 09:59:41 -0300

    feat (overview): build template

--- src/controllers/viewController.js
+++ src/controllers/viewController.js
@@ -1,8 +1,19 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/TourModel');
+const catchAsync = require('../Utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render the template using data from 1)
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
