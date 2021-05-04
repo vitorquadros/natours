55c5233e94d540df5efc1eeb5e73f4438bf1f109
vitor <viitorquadros@hotmail.com>
2021-05-04 09:40:41 -0300

    feat (views): overview/tour blocks

--- app.js
+++ app.js
@@ -72,6 +72,18 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The forest hiker tour',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
