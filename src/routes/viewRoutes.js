62467bba32a1f4b489d70269ae081a9f46ad1173
vitor <viitorquadros@hotmail.com>
2021-05-05 18:22:04 -0300

    feat (login): render logged in view

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -4,10 +4,10 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.isLoggedIn);
+
 router.get('/', viewController.getOverview);
-
-router.get('/tour/:slug', authController.protect, viewController.getTour);
-
+router.get('/tour/:slug', viewController.getTour);
 router.get('/login', viewController.getLoginForm);
 
 module.exports = router;
