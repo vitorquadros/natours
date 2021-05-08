b162b2fe0190f56b2f18f177fe49a7dd13af0124
vitor <viitorquadros@hotmail.com>
2021-05-08 18:24:25 -0300

    feat (account info): update user info with api

--- src/controllers/viewController.js
+++ src/controllers/viewController.js
@@ -50,21 +50,21 @@ exports.getAccount = (req, res) => {
   });
 };
 
-exports.updateUserData = catchAsync(async (req, res, next) => {
-  const updatedUser = await User.findByIdAndUpdate(
-    req.user.id,
-    {
-      name: req.body.name,
-      email: req.body.email,
-    },
-    {
-      new: true,
-      runValidators: true,
-    }
-  );
+// exports.updateUserData = catchAsync(async (req, res, next) => {
+//   const updatedUser = await User.findByIdAndUpdate(
+//     req.user.id,
+//     {
+//       name: req.body.name,
+//       email: req.body.email,
+//     },
+//     {
+//       new: true,
+//       runValidators: true,
+//     }
+//   );
 
-  res.status(200).render('account', {
-    title: 'Your account',
-    user: updatedUser,
-  });
-});
+//   res.status(200).render('account', {
+//     title: 'Your account',
+//     user: updatedUser,
+//   });
+// });
