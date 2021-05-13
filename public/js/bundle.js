02b7042530464e52e944ac806879e14599e5ded3
vitor <viitorquadros@hotmail.com>
2021-05-13 13:33:49 -0300

    feat (bookings): model db

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9144,7 +9144,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34485" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34839" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
