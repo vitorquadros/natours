691c64cb72a74107a3a6b218787f9b45dd89ca41
vitor <viitorquadros@hotmail.com>
2021-05-14 11:03:02 -0300

    add compression

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9064,7 +9064,6 @@ if (userDataForm) {
     form.append('name', document.getElementById('name').value);
     form.append('email', document.getElementById('email').value);
     form.append('photo', document.getElementById('photo').files[0]);
-    console.log(form);
     (0, _updateSettings.updateSettings)(form, 'data');
   });
 }
