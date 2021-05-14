1e60897ecaac5afd98bd797fdb570eeb00d288e6
vitor <viitorquadros@hotmail.com>
2021-05-14 10:23:47 -0300

    feat (bookings): admin crud

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -46,3 +46,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.getAllBookings = factory.getAll(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.createBooking = factory.createOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
